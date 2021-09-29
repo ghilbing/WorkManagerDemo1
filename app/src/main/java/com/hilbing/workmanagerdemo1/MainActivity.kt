1c00287073a5b801c31d878b72230fb73c1cf9cf
Gretel Hilbing <ghilbing@gmail.com>
2021-09-29 07:12:21 -0700

    Get Status from Worker

--- app/src/main/java/com/hilbing/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/hilbing/workmanagerdemo1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hilbing.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,8 +17,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOnTimeWorkRequest(){
+        val workManager: WorkManager = WorkManager.getInstance(applicationContext)
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
-        WorkManager.getInstance(applicationContext)
-            .enqueue(uploadRequest)
+        workManager.enqueue(uploadRequest)
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this, Observer {
+            textView.text = it.state.name
+        })
+
     }
 }
